f6d419818477ea9d269780c05c794235e5ad7196
RakhaShidqi <rakhashidqi628@gmail.com>
2024-07-07 21:55:23 +0700

    Penambahan fitur produk dan fungsional tombol search

--- UAS/js/script.js
+++ UAS/js/script.js
@@ -1,4 +1,4 @@
-//Toggle Class Active
+//Toggle Class Active untuk Hamburger menu
 const navbarNav = document.querySelector
 ('.navbar-nav');
 // Ketika Hamburger menu di klik
@@ -7,12 +7,65 @@ onclick = () => {
     navbarNav.classList.toggle('active');
 };
 
-//  klik di luar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector
-('#hamburger-menu');
+// Toggle class active untuk search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// toggle Class active untuk Shopping cart
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+  shoppingCart.classList.toggle('active');
+  e.preventDefault();
+};
+
+
+//  klik di luar elemen
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+const scb = document.querySelector('#shopping-cart-button');
 
 document.addEventListener('click',function(e) {
-  if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if(!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove('active');
-  }  
-})
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+  
+  if (!scb.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
+
+});
+
+// Modal Box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+itemDetailModal.style.display = "flex";
+    e.preventDefault;
+  };
+  });
+
+
+// Klik tombol Close modal
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = 'none';
+  e.preventDefault();
+};
+
+// klik di luar modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+}
